aeb16e73352cec44ad6046d8010ed58015ee2278
EHU <a@a.com>
2022-11-16 15:21:48 +0100

    2 PAGINAS DEL JSF

---
+++ src/main/java/NavegationController.java
@@ -0,0 +1,12 @@
+
+public class NavegationController {
+	public String QueryQuestion() {
+		return "QueryQuestion";
+	}
+	public String CreateQuery() {
+		return "CreateQuery";
+	}
+	public String Index() {
+		return "Index";
+	}
+}
